3734bd5e5a92036ba3af1047a0a3314678670056
Isachi25 <sharonachiengmasiga@gmail.com>
2024-02-24 13:25:21 -0800

    Sorts doubly linked lists of integers in ascending order

--- sort.h
+++ sort.h
@@ -22,7 +22,8 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quickSort(int *array, int low, int high, size_t size);
 int partition(int *array, int low, int high, size_t size);
-void swap(int *a, int *b);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void swap(listint_t *a, listint_t *b);
 
 #endif
